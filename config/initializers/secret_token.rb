f77c949c79c510a58bab5a815e46da79946fecd5
ShivaniMete <shivani-mete@uiowa.edu>
2013-11-05 09:25:08 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FTCLeagues::Application.config.secret_token = '86bf122734718d43fdf507f4280fa1653db2ca69e8f0c20155bd23972354c6be5e9dc6c3d64174681a384faa893f995bd7e0612a28d256d38a0903709be81a09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FTCLeagues::Application.config.session_store :cookie_store, key: '_FTC-Leagues_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FTCLeagues::Application.config.session_store :active_record_store
