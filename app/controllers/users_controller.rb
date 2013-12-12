e5512b89e89cb0d27adf7d4a94a7e12d97c0726d
justgottrickd <patrick-walsh@uiowa.edu>
2013-12-11 19:36:15 -0800

    Fixed Image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,8 @@ def create
 end
 
 def show
-<<<<<<< HEAD
-@league = League.find_by_league_admin(params[:id])
-
-=======
  check_access_user_Admins
  @league = League.find_by_league_admin(params[:id])
->>>>>>> 10aa0673f8947e4394f448a056fc42d3e1dd4dda
  #default page
 end
 
