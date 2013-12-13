515812192342a78b70233558e8116997ba03a22c
rgndhi <richa-gandhi@uiowa.edu>
2013-12-12 20:20:31 -0800

    last iteration

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-
-module FTCLeagues
+config.assets.initialize_on_precompile = false
+module FTCRobotics
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
